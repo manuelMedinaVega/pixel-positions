4b57d499b2bd4c34eb2ec589411a2221e9a0f26e
manuelMedinaV <manuelmedinamtg@gmail.com>
2024-09-01 21:05:23 -0300

    joda

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,7 +2,7 @@
     <div class="space-y-10">
 
         <section class="text-center pt-6">
-            <h1 class="font-bold text-4xl">Let's find your next job</h1>
+            <h1 class="font-bold text-4xl">Pibe gay</h1>
 
             <x-forms.form action="/search" class="mt-6">
                 <x-forms.input :label="false" name="q" placeholder="Web Developer..." />
